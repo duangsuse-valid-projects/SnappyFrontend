792f46c4f8ad6a69ca459c414c018fc0bbbdcfbf
duangsuse <fedora-opensuse@outlook.com>
2018-10-02 01:25:21 +0800

    Update source

--- main.rs
+++ main.rs
@@ -9,7 +9,11 @@ extern crate core;
 
 mod snappy;
 
-use libc::printf;
+use libc::{printf, fprintf, open};
+use libc::{strcmp, strcat};
+use libc::exit;
+use libc::mmap;
+
 use core::panic::PanicInfo;
 
 #[no_mangle]
@@ -20,9 +24,39 @@ use core::panic::PanicInfo;
 ///
 /// * unsnappy \[file\] \[output\]
 /// uncompress file to output, which default is stdout
-pub unsafe extern "C" fn _main(argc: i32, argv: *const *const u8) -> i32 {
-  printf("Hello, world!\n\00".as_ptr() as *const i8);
+pub unsafe extern "C" fn _main(argc: i32, argv: *const [*const i8]) -> i32 {
+  if argc == 1 {
+    print("snappy [files]\ncompress each file with suffix .snappy, `-' means stdin\n\nunsnappy [file] [output]\nuncompress file to output, which default is stdout\n\00");
+    exit(1);
+  }
+
+  if strcmp((*argv)[0], "snappy\00".as_ptr() as *const i8) == 0
+  || strcmp((*argv)[0], "./snappy\00".as_ptr() as *const i8) == 0
+  {
+    print("Snappy running in zip mode\n\00");
+
+    snappy_zip(argc - 1, argv);
+  } else {
+    print("Snappy running in unzip mode\n\00");
+
+    if argc > 3 { print("Too much arguments given\n\00"); exit(1); }
+    let o = if argc == 2 { 1 } else { open((*argv)[2], libc::O_WRONLY) };
+
+    snappy_unzip((*argv)[1], o);
+  }
+
   return 0;
+}
+
+pub unsafe extern "C" fn snappy_zip(num: i32, pathes: *const [*const i8]) {
+}
+
+pub unsafe extern "C" fn snappy_unzip(input: *const i8, output: i32) {
+}
+
+/// Display rust &str to stdout
+pub unsafe fn print(str: &str) {
+  printf(str.as_ptr() as *const i8);
 }
 
 #[lang = "eh_personality"]
